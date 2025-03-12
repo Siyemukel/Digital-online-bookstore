05023784250f5fac32122387acca50070a07bdad
Skhumbuzo Mkize <132460543+SkhumbuzoMkize@users.noreply.github.com>
2025-03-12 09:31:41 -1100

    Add files via upload
    I am still in progress....

---
+++ Webpage/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bookTable = document.getElementById("bookTable");
+    const addBookButton = document.getElementById("addBookButton");
+    
+    });
+    
+    
