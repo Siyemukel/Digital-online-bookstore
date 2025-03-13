2114afe3e069b9646b8c760b716162cd18b74b19
Xolani Shabangu <142493075+XolaniShabangu@users.noreply.github.com>
2025-03-13 10:13:59 +0200

    Delete Webpage directory

--- Webpage/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const bookTable = document.getElementById("bookTable");
-    const addBookButton = document.getElementById("addBookButton");
-    
-    });
-    
-    
